141834cfde3aff4eca0555556791e0dee8244b44
jeansn <jeansn@tfg.co.za>
2020-10-20 05:48:39 +0200

    completed, before hooks and context refactor

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -40,6 +41,14 @@ class App extends Component {
     this.setState({ loading: false, user: res.data });
   };
 
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await Axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ loading: false, repos: res.data });
+  };
+
   clearUsers = () => {
     this.setState({ loading: false, users: [] });
   };
@@ -52,7 +61,7 @@ class App extends Component {
   };
 
   render() {
-    const { user, users, loading } = this.state;
+    const { user, users, repos, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -82,7 +91,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,22 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Spinner from "../layouts/Spinner";
+import Repos from "../../components/repos/Repos";
 import Proptypes from "prop-types";
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: Proptypes.bool,
     user: Proptypes.object.isRequired,
+    repos: Proptypes.array.isRequired,
     getUser: Proptypes.func.isRequired,
+    getUserRepos: Proptypes.func.isRequired,
   };
 
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -39,6 +44,69 @@ class User extends Component {
         <Link to="/" className="btn btn-light">
           Back to Search
         </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i classname="fas.fa-times-circle.text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              alt=""
+              className="round-img"
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>
+                  <strong>Bio</strong>
+                </h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-primary">
+            Public Gists: {public_gists}
+          </div>
+        </div>
+        <Repos repos={this.props.repos} />
       </div>
     );
   }
